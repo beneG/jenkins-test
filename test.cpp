f6247cf33217ba52dc296a7b691c6afe4785ae11
Nurbek Asanbekov <nasanbekov@gmail.com>
2017-04-05 11:59:50 +0600

    second commit

--- test.cpp
+++ test.cpp
@@ -6,7 +6,8 @@ int main()
 {
     std::map<std::string, std::string> m = {{"1", "one"}, {"2", "two"}, {"3", "three"}, {"4", "four"}};
     m["10"] = "ten";
-    std::cout << m["4"] << std::endl;
+    std::cout << m["10"] << std::endl;
+    m["10"] = "dec";
     std::cout << m["10"] << std::endl;
     return 0;
 }
